6426933eae001fe16aff7d07630b5f4440617e3d
ryanwmceachern <105886833+ryanwmceachern@users.noreply.github.com>
2022-12-21 15:37:13 -0400

    finished newsletter app, entity framework , and car insurance app

---
+++ NewsLetter1/NewsLetter1/Controllers/AdminController.cs
@@ -0,0 +1,72 @@
+﻿using NewsLetter1.Models;
+using NewsLetter1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsLetter1.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                //var SignUps = db.SignUps.Where(x => x.Removed == null).ToList();
+                var SignUps = (from c in db.SignUps
+                               where c.Removed == null
+                               select c).ToList();
+                var signupVms = new List<SignupVm>();
+                foreach (var SignUp in SignUps)
+                {
+                    var signupVm = new SignupVm();
+                    signupVm.Id = SignUp.Id;
+                    signupVm.FirstName = SignUp.FirstName;
+                    signupVm.LastName = SignUp.LastName;
+                    signupVm.EmailAddress = SignUp.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+                return View(signupVms);
+            }
+        }    
+        
+            //string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber from SignUps";
+            //List<NewsletterSignUp> SignUps = new List<NewsletterSignUp>();
+
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    SqlCommand command = new SqlCommand(queryString, connection);
+
+            //    connection.Open();
+
+            //    SqlDataReader reader = command.ExecuteReader();
+
+            //    while (reader.Read())
+            //        {
+            //        var signup = new NewsletterSignUp();
+            //        signup.Id = Convert.ToInt32(reader["Id"]);
+            //        signup.FirstName = reader["FirstName"].ToString();
+            //        signup.LastName = reader["LastName"].ToString();
+            //        signup.EmailAddress = reader["EmailAddress"].ToString();
+            //        signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
+            //        SignUps.Add(signup);
+            //        }
+            //}
+
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                 var signup = db.SignUps.Find(Id);
+                     signup.Removed = DateTime.Now;
+                     db.SaveChanges();
+
+            }
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
--- NewsLetter1/NewsLetter1/Controllers/HomeController.cs
+++ NewsLetter1/NewsLetter1/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace NewsLetter1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly string connectionString = @"Data Source=LAPTOP-JTPJE0DT\SQLEXPRESS;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //private readonly string connectionString = @"Data Source=LAPTOP-JTPJE0DT\SQLEXPRESS;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
        
         public ActionResult Index()
         {
@@ -29,68 +29,39 @@ namespace NewsLetter1.Controllers
             }
             else
             {
-                
-                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
-                                            (@FirstName, @LastName, @EmailAddress)";
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using(NewsletterEntities db = new NewsletterEntities())
                 {
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                    var signup = new SignUp();
+                    signup.FirstName = FirstName;
+                    signup.LastName = LastName;
+                    signup.EmailAddress = EmailAddress;
 
-                    command.Parameters["@FirstName"].Value = FirstName;
-                    command.Parameters["@LastName"].Value = LastName;
-                    command.Parameters["@EmailAddress"].Value = EmailAddress;
-
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
 
                 }
+                //string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                //                            (@FirstName, @LastName, @EmailAddress)";
+                //using (SqlConnection connection = new SqlConnection(connectionString))
+                //{
+                //    SqlCommand command = new SqlCommand(queryString, connection);
+                //    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                //    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                //    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                //    command.Parameters["@FirstName"].Value = FirstName;
+                //    command.Parameters["@LastName"].Value = LastName;
+                //    command.Parameters["@EmailAddress"].Value = EmailAddress;
+
+                //    connection.Open();
+                //    command.ExecuteNonQuery();
+                //    connection.Close();
+
+                //}
                 return View("Success");
             }
         }
         
-        public ActionResult Admin()
-        {
-         using (NewsletterEntities db = new NewsletterEntities())
-            {
-                var SignUps = db.SignUps;
-                var signupVms = new List<SignupVm>();
-                foreach (var SignUp in SignUps)
-                {
-                    var signupVm = new SignupVm();
-                    signupVm.FirstName = SignUp.FirstName;
-                    signupVm.LastName = SignUp.LastName;
-                    signupVm.EmailAddress = SignUp.EmailAddress;
-                    signupVms.Add(signupVm);
-                }
-                return View(signupVms);
-            }
-            //string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber from SignUps";
-            //List<NewsletterSignUp> SignUps = new List<NewsletterSignUp>();
-
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    SqlCommand command = new SqlCommand(queryString, connection);
-
-            //    connection.Open();
-
-            //    SqlDataReader reader = command.ExecuteReader();
-
-            //    while (reader.Read())
-            //        {
-            //        var signup = new NewsletterSignUp();
-            //        signup.Id = Convert.ToInt32(reader["Id"]);
-            //        signup.FirstName = reader["FirstName"].ToString();
-            //        signup.LastName = reader["LastName"].ToString();
-            //        signup.EmailAddress = reader["EmailAddress"].ToString();
-            //        signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
-            //        SignUps.Add(signup);
-            //        }
-            //}
-          
-        }
+       
     }
 }
